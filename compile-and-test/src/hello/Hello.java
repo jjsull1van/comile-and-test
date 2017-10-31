36db9e79470c9d87e06bb6bfd3abea86ffb82559
jjsull1van <jim@arcisphere.com>
2017-10-31 06:57:48 -0700

    good

---
+++ compile-and-test/src/hello/Hello.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ *Licensed Materials - Property of IBM
+ *(c) Copyright IBM Corporation 2008. All Rights Reserved.
+ *
+ *Note to U.S. Government Users Restricted Rights:  
+ *Use, duplication or disclosure restricted by GSA ADP Schedule 
+ *Contract with IBM Corp. 
+ *******************************************************************************/
+package hello;
+
+public class Hello {
+    public String getMessage() {
+        return "Hello";
+    }
+}
---
+++ compile-and-test/src/hello/test/HelloTest.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *Licensed Materials - Property of IBM
+ *(c) Copyright IBM Corporation 2008. All Rights Reserved.
+ *
+ *Note to U.S. Government Users Restricted Rights:  
+ *Use, duplication or disclosure restricted by GSA ADP Schedule 
+ *Contract with IBM Corp. 
+ *******************************************************************************/
+package hello.test;
+
+import junit.framework.TestCase;
+import hello.Hello;
+
+public class HelloTest extends TestCase {
+    public void testGetMessage() {
+        Hello hello = new Hello();
+        assertEquals("Hello", hello.getMessage());
+    }
+}
